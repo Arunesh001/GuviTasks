7501a14f58cbb598512f0d06ab45e82b2f269b5d
Arunesh001 <129960351+Arunesh001@users.noreply.github.com>
2025-02-07 12:41:25 +0530

    Update Person_1Main.java

--- src/GuviTask2/Person_1Main.java
+++ src/GuviTask2/Person_1Main.java
@@ -3,6 +3,7 @@ package GuviTask2;
 public class Person_1Main {
     public static void main(String[] args) {
         Employee obj = new Employee("Aruneshwaran", 23, 102, 50000);
+        System.out.println("This is actual name and age : ");
         obj.display();
     }
 }
